6d5809a32b498700e2e4a3d2e29ab468792338f5
Elliot Wolk <elliot.wolk@gmail.com>
2015-08-08 16:51:13 -0400

    email-search.pl: add runSql() sub to run sqlite against a folder-specific db

--- src/email-search.pl
+++ src/email-search.pl
@@ -1,11 +1,40 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Time::HiRes qw(time);
+
+sub runSql($$$);
+
+my $emailDir = "$ENV{HOME}/.cache/email";
 
 my $usage = "Usage:
 ";
 
 sub main(@){
+}
+
+sub runSql($$$){
+  my ($accName, $folderName, $sql) = @_;
+  my $db = "$emailDir/$accName/$folderName/db";
+
+  $sql =~ s/\s*;\s*\n*$//;
+  $sql = "$sql;\n";
+
+  my $nowMillis = int(time*1000);
+  my $tmpSqlFile = "/tmp/email-$accName-$folderName-$nowMillis.sql";
+  open TMPFH, "> $tmpSqlFile";
+  print TMPFH $sql;
+  close TMPFH;
+
+  my @cmd = ("sqlite3", $db, ".read $tmpSqlFile");
+  open SQLITECMD, "-|", @cmd;
+  my @lines = <SQLITECMD>;
+  close SQLITECMD;
+  die "error running @cmd\n" if $? != 0;
+
+  system "rm", $tmpSqlFile;
+
+  return join '', @lines;
 }
 
 &main(@ARGV);
