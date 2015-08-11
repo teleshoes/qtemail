44700438febaabc6ed9786dea2fab2c6755bd2fb
Elliot Wolk <elliot.wolk@gmail.com>
2015-08-11 14:51:41 -0400

    usage_to_readme: /usr/bin/email.pl => /opt/qtemail/bin/email.pl

--- usage_to_readme.pl
+++ usage_to_readme.pl
@@ -5,9 +5,10 @@ use warnings;
 sub main(@){
   my $readme = `cat README`;
   $readme =~ s/\nUsage:\n.*//sg;
-  my $usage = `export HOME="~"; email.pl -h 2>&1`;
+  my $emailUsage = `export HOME="~"; /opt/qtemail/bin/email.pl -h 2>&1`;
+  $emailUsage =~ s/^Usage:\s*\n//;
   open FH, "> README";
-  print FH "$readme\nUsage:\n$usage";
+  print FH "$readme\nUsage:\n$emailUsage";
   close FH;
 }
 
