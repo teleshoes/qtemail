2b09d2f5f1c2b887494e06817cc505063452b8ad
Elliot Wolk <elliot.wolk@gmail.com>
2015-08-11 14:57:42 -0400

    usage_to_readme: add usage for email-search.pl

--- usage_to_readme.pl
+++ usage_to_readme.pl
@@ -6,9 +6,18 @@ sub main(@){
   my $readme = `cat README`;
   $readme =~ s/\nUsage:\n.*//sg;
   my $emailUsage = `export HOME="~"; /opt/qtemail/bin/email.pl -h 2>&1`;
-  $emailUsage =~ s/^Usage:\s*\n//;
+  $emailUsage =~ s/^(Usage:)?(\s*\n)*//;
+  $emailUsage =~ s/\n*$/\n/;
+  my $emailSearchUsage = `export HOME="~"; /opt/qtemail/bin/email-search.pl -h 2>&1`;
+  $emailSearchUsage =~ s/^(Usage:)?(\s*\n)*//;
+  $emailSearchUsage =~ s/\n*$/\n/;
   open FH, "> README";
-  print FH "$readme\nUsage:\n$emailUsage";
+  print FH "$readme\nUsage:\n";
+  print FH "===== email.pl =====\n\n";
+  print FH $emailUsage;
+  print FH "\n";
+  print FH "===== email-search.pl =====\n\n";
+  print FH $emailSearchUsage;
   close FH;
 }
 
