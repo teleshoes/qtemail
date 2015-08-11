4a8bf8f621ec235825aef7eba2c4e62ad22b37ec
Elliot Wolk <elliot.wolk@gmail.com>
2015-08-11 14:59:27 -0400

    usage_to_readme: add usage for email-gui.py

--- usage_to_readme.pl
+++ usage_to_readme.pl
@@ -11,6 +11,9 @@ sub main(@){
   my $emailSearchUsage = `export HOME="~"; /opt/qtemail/bin/email-search.pl -h 2>&1`;
   $emailSearchUsage =~ s/^(Usage:)?(\s*\n)*//;
   $emailSearchUsage =~ s/\n*$/\n/;
+  my $emailGuiUsage = `export HOME="~"; /opt/qtemail/bin/email-gui.py -h 2>&1`;
+  $emailGuiUsage =~ s/^(Usage:)?(\s*\n)*//;
+  $emailGuiUsage =~ s/\n*$/\n/;
   open FH, "> README";
   print FH "$readme\nUsage:\n";
   print FH "===== email.pl =====\n\n";
@@ -18,6 +21,9 @@ sub main(@){
   print FH "\n";
   print FH "===== email-search.pl =====\n\n";
   print FH $emailSearchUsage;
+  print FH "\n";
+  print FH "===== email-gui.py =====\n\n";
+  print FH $emailGuiUsage;
   close FH;
 }
 
